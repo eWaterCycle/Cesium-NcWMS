887362cda9595841a768a8a484e4dfaf40861ed6
maartenvm <m.vanmeersbergen@esciencecenter.nl>
2014-07-31 13:13:33 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pattyVis2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pattyVis2App
+ */
+angular.module('pattyVis2App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pattyVis2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pattyVis2App
+ */
+angular.module('pattyVis2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
