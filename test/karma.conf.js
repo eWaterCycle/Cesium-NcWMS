49f16cada166d55a9cffc4ac06955b4af56045aa
maartenvm <m.vanmeersbergen@esciencecenter.nl>
2015-02-10 11:42:31 +0100

    Added dependencies (cesium, world-countries).

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/cesiumjs/Cesium/Cesium.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
