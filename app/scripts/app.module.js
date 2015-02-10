362bfa9bc164171b510122f3b77357c2ed85762e
maartenvm <m.vanmeersbergen@esciencecenter.nl>
2015-02-10 11:54:00 +0100

    Added app/scripts/app.module.js

---
+++ app/scripts/app.module.js
@@ -0,0 +1,3 @@
+
+
+angular.module('eWaterCycleApp', [ 'ui.bootstrap' ]);
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/cesiumjs/Cesium/Cesium.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
