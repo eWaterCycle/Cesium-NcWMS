f2ffbeb63a456e74dc28af229665e00491d2a11a
Maartenvm <m.vanmeersbergen@esciencecenter.nl>
2015-04-13 15:55:16 +0200

    Changed stats group visualizations to use the mean value for the map.

--- app/scripts/dataset/dataset.controller.js
+++ app/scripts/dataset/dataset.controller.js
@@ -8,26 +8,26 @@
       return NcwmsService.datasets;
     };
 
-    this.selectedDataset = 'default';
-    Messagebus.subscribe('ncwmsDatasetSelected', function(event, value) {
-      if (this.selectedDataset !== value) {
-        this.selectedDataset = value;
-      }
-    }.bind(this));
 
     this.selectDataset = function(dataset) {
       Messagebus.publish('ncwmsDatasetSelected', dataset);
-      Messagebus.publish('ncwmsUnitsChange', dataset.units);
-      Messagebus.publish('legendMinChange', dataset.min);
-      Messagebus.publish('legendMaxChange', dataset.max);
 
-      if (dataset.graphicalMin !== 0) {
-        Messagebus.publish('graphMinChange', dataset.graphicalMin);
-        Messagebus.publish('graphMaxChange', dataset.graphicalMax);
-      } else {
-        Messagebus.publish('graphMinChange', dataset.min);
-        Messagebus.publish('graphMaxChange', dataset.max);
+      var datasetForMap = this.selectedDataset;
+      if (this.selectedDataset.statsGroup) {
+        datasetForMap = this.selectedDataset.datasetMean;
       }
+
+      Messagebus.publish('ncwmsUnitsChange', datasetForMap.units);
+      Messagebus.publish('legendMinChange', datasetForMap.min);
+      Messagebus.publish('legendMaxChange', datasetForMap.max);
+
+      // if (dataset.graphicalMin !== 0) {
+      //   Messagebus.publish('graphMinChange', dataset.graphicalMin);
+      //   Messagebus.publish('graphMaxChange', dataset.graphicalMax);
+      // } else {
+      //   Messagebus.publish('graphMinChange', dataset.min);
+      //   Messagebus.publish('graphMaxChange', dataset.max);
+      // }
     };
   }
 
