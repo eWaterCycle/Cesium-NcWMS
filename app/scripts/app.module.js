045b98d485027e341b51c824069c6bbff5adb8b0
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-03-12 13:34:48 +0100

    Allow app to be hosted on https

--- app/scripts/app.module.js
+++ app/scripts/app.module.js
@@ -92,7 +92,7 @@
         var scriptTag = $document[0].createElement('script');
         scriptTag.type = 'text/javascript';
         scriptTag.async = true;
-        scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+        scriptTag.src = '//d3js.org/d3.v3.min.js';
         scriptTag.onreadystatechange = function() {
           if (this.readyState === 'complete') {
             onScriptLoad();
